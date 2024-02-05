1f4c236899b3ff6c247db9e0cba9d3afdc59ad71
PSriVignesH <srivigneshsri1999@gmail.com>
2024-02-05 14:23:58 +0530

    Type adding in getFavoriteListings

--- app/actions/getFavoriteListings.ts
+++ app/actions/getFavoriteListings.ts
@@ -18,7 +18,7 @@ export default async function getFavoriteListings(){
     })
 
 
-    const safeFavorites = favorites.map((favorite)=>{
+    const safeFavorites = favorites.map((favorite:any)=>{
       return {
         ...favorite,
         createdAt:favorite.createdAt.toString()
